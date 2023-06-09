c9a93a6b037424d1cc0028fca0314f57424a1118
egekulahli <egekulahli@gmail.com>
2023-06-09 22:27:59 +0300

    initial changes

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,88 +1,38 @@
 import React from "react";
-import { useForm } from "react-hook-form";
-import { nanoid } from "nanoid";
-import { useHistory } from "react-router";
-import Gratitude from "./../assets/grForm.png";
+import { formatDistanceToNow } from "date-fns";
+import { tr } from "date-fns/locale";
+import { useDispatch } from "react-redux";
+import { notSilAPI } from "../actions";
+import { toast } from "react-toastify";
 
-export default function PostForm() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({ mode: "onChange" });
-
-  const history = useHistory();
-
-  function onSubmit(data) {
-    const yeniNot = {
-      id: nanoid(),
-      date: Date(),
-      body: Object.values(data)
-        .filter((v) => v !== "")
-        .join("|"),
-    };
-
-    // burada ilgili eylemi dispatch edin
-    // toast mesajı gösterin
-    // sonra aşağıdaki satırı aktifleştirin
-    // setTimeout(() => history.push("/notlar"), 2000);
+export default function Post({ item }) {
+  const dispatch = useDispatch();
+  function handleSil() {
+    dispatch(notSilAPI(item.id));
+    toast("Notunuz silindi!", { type: "error" });
   }
 
-  const inputCx = "border border-zinc-300 h-9 rounded-none text-sm px-2 w-full";
-
   return (
-    <div className="flex flex-col sm:flex-row beyazKutu">
-      <div className="flex-1">
-        <img src={Gratitude} alt="" className="block object-cover h-full" />
-      </div>
+    <div className="beyazKutu p-8 pb-6 mb-4 text-sm">
+      <h1>
+        {formatDistanceToNow(new Date(item.date), {
+          addSuffix: true,
+          locale: tr,
+        })}
+      </h1>
 
+      {item.body.split("|").map((li) => (
+        <p className="mt-2" key={li}>
+          - {li}
+        </p>
+      ))}
 
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col gap-3 p-8 flex-1"
+      <button
+        className="text-xs text-amber-600 mt-4 underline"
+        onClick={handleSil}
       >
-        <h1>Minnettar hissediyorum, çünkü...</h1>
-        <p className="text-xs">
-          Minnettar günlüğü notları; her gün teşekkür edilen birkaç şeyi
-          listelemekten, minnettar olunan şeylere dair daha uzun ve kapsamlı
-          yansıtmalara kadar pek çok şeyden oluşabilir.
-        </p>
-        <p className="text-stone-700 my-3 text-xs">
-          Her gün belli saatlerde 3 maddeden oluşan bir liste
-          yapmak, bu alışkanlığa iyi bir başlangıç noktası sayılır.
-        </p>
-        <div>
-          <input
-            className={inputCx}
-            placeholder="Dışarıda tam en sevdiğim hava var"
-            {...register("g1", { required: "Bu alan zorunludur" })}
-          />
-          {errors.g1 && (
-            <p className="text-sm text-rose-700 py-1">{errors.g1.message}</p>
-          )}
-        </div>
-        <div>
-          <input
-            className={inputCx}
-            placeholder="Kedim beni bu sabah çok erken uyandırmadı :D"
-            {...register("g2")}
-          />
-        </div>
-        <div>
-          <input
-            className={inputCx}
-            placeholder="Sevdiğim kurabiyeleri satan dükkan bugün açık"
-            {...register("g3")}
-          />
-        </div>
-
-        <button
-          type="submit"
-          className="myButton"
-        >
-          Ekle
-        </button>
-      </form>
+        Bu notu sil
+      </button>
     </div>
   );
 }
